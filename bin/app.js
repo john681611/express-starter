f1699711bfb1481a96a95e50035fc8aff0da4461
john681611 <john.harvey@marks-and-spencer.com>
2018-12-03 11:31:01 +0000

    refactor

--- bin/app.js
+++ bin/app.js
@@ -1,26 +1,27 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var glob = require('glob');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'..', 'src', 'pages'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'src', 'common')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+glob.sync(__dirname+'/../src/pages/**/route.js').forEach(page => {
+    app.use(require(page));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +36,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error/error');
 });
 
 module.exports = app;
--- bin/www.js
+++ bin/www.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('express-starter:server');
 var http = require('http');
 
