3262bf42fb03df074b1fe1ad4ab4cc2dc8c3027d
john681611 <john.harvey@marks-and-spencer.com>
2019-01-30 11:53:38 +0000

    more items

--- src/common/javascripts/pwa.js
+++ src/common/javascripts/pwa.js
@@ -1,6 +1,17 @@
 // #1
+
 // if ('serviceWorker' in navigator) {
 //     navigator.serviceWorker
 //         .register('../service-worker.js')
 //         .then(function() { console.log('Service Worker Registered'); });
 //   }
+
+// #7
+
+// if ('PushManager' in window) {
+//     Notification.requestPermission(function (permission) {
+//         if (permission === "granted") {
+//         var notification = new Notification("Hi there!");
+//         }
+//     })
+// };
--- src/common/service-worker.js
+++ src/common/service-worker.js
@@ -1,9 +1,11 @@
 // #1
+
 // self.addEventListener('install', function(e) {
 //     console.log('[ServiceWorker] Installed');
 // });
 
 // #2
+
 // importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
 // if (workbox) {
@@ -13,28 +15,21 @@
 // }
 
 // #3
+
 // workbox.routing.registerRoute(
-//     // Cache CSS files
 //     /.*\.css/,
-//     // Use cache but update in the background ASAP
 //     workbox.strategies.staleWhileRevalidate({
-//       // Use a custom cache name
 //       cacheName: 'css-cache',
 //     })
 //   );
   
 //   workbox.routing.registerRoute(
-//     // Cache image files
 //     /.*\.(?:png|jpg|jpeg|svg|gif)/,
-//     // Use the cache if it's available
 //     workbox.strategies.cacheFirst({
-//       // Use a custom cache name
 //       cacheName: 'image-cache',
 //       plugins: [
 //         new workbox.expiration.Plugin({
-//           // Cache only 20 images
 //           maxEntries: 20,
-//           // Cache for a maximum of a week
 //           maxAgeSeconds: 7 * 24 * 60 * 60,
 //         })
 //       ],
@@ -42,6 +37,7 @@
 //   );
 
 // #4
+
 // workbox.precaching.precacheAndRoute([
 //     '/stylesheets/style.css',
 //     '/javascripts/pwa.js',
@@ -49,20 +45,22 @@
 // ]);
 
 // #5
+
 // self.addEventListener('push', function(event) {
 //     if (event.data) {
 //       console.log('This push event has data: ', event.data.text());
 
-        // #6
-        // return self.registration.showNotification('LISTEN TO ME', {
-        //     body: event.data.text()
-        // });
+//         // #6
+//         return self.registration.showNotification('LISTEN TO ME', {
+//             body: event.data.text()
+//         });
 //     } else {
 //       console.log('This push event has no data.');
 //     }
 //   });
 
 // #7
+
 // self.addEventListener('notificationclick', function(event) {
 //     if (!event.action) {
 //       // Was a normal notification click
